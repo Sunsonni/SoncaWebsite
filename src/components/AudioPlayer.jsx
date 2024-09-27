49340c41b6c13e8e36f8e733ba44095c197e25fa
sunsonni <sonniemnguyen@gmail.com>
2024-09-26 19:58:23 -0500

    more formating

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -4,7 +4,7 @@ const AudioPlayer = () => {
     return (
         // <Draggable>
             <div className="audio-player">
-             <iframe style={{ borderRadius:'12px'}} src="https://open.spotify.com/embed/playlist/2GXhCZgL7qKpSe9OVPZmk5?utm_source=generator" width="80%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+             <iframe className='spotify-player'style={{ borderRadius:'12px'}} src="https://open.spotify.com/embed/playlist/2GXhCZgL7qKpSe9OVPZmk5?utm_source=generator" width="80%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </div>
         // </Draggable>
     );
